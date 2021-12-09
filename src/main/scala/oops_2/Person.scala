570af875cfce3cd5b7bba3a8c9019a31a1b6828c
Shivam <shivam.mehta@knoldus.com>
2021-12-09 10:16:59 +0530

    OOPs Assignment

---
+++ src/main/scala/oops_2/Person.scala
@@ -0,0 +1,24 @@
+package oops_2
+
+class Person(var name:String,var age:Int) extends Ordered[Person] {
+   override def compare(that: Person): Int = {
+    this.name compare that.name match {
+      case 0 =>  if(this.age.equals(that.age))
+                    println("True\nName & age both are same")
+                  else
+                    println("False\nName are same but ages are not equal")
+                  0
+
+      case _ =>  if(this.name.length.equals(that.name.length) && this.age.equals(that.age))
+                        println("True")
+                      else println("false")
+                      1
+
+    }
+  }
+}
+object Person extends  App{
+  var personOne = new Person("Shivam",21)
+  var personTwo = new Person("Hardik",24)
+  personOne compare personTwo
+}
